2f9cdfa1089de2cd9c31010415c3e9474298faa7
Stefan Cosma <stefan.cosma@ropardo.ro>
2023-04-20 13:55:08 +0300

    Partial lint fix

--- README.md
+++ README.md
@@ -7,22 +7,20 @@ In other words it's a small Python script that clears a specific Plex Media Serv
 
 **PSA: Run Plexorcist only on a Plex Media Server to which you have access and are certain you want to clean a specific library. The creator of this script is not responsible for any unintended data loss.**
 
-
 ## Features
 
 - Suppport for Movies or TV Shows
 - JSON config file
 - I18N (via config file) - [feeling fancy?](https://github.com/stefanbc/Plexorcist/wiki/I18N---King-James-Version)
 - Log file
-    - Automatic creation and cleanup when file gets too big (2 MB)
-    - Logs the timestamp for all actions
-    - Logs the name of the Movie or TV Show (including episode title)
-    - Logs the space reclaimed after deletion
+  - Automatic creation and cleanup when file gets too big (2 MB)
+  - Logs the timestamp for all actions
+  - Logs the name of the Movie or TV Show (including episode title)
+  - Logs the space reclaimed after deletion
 - IFTTT integration (via webhook)
 - Movies or TV Shows whitelist
 - Dry run the script and output the result in console
 - Requests error handling
-
 
 ## Installation
 
@@ -52,11 +50,9 @@ Give permissions to Plexorcist
 
 Before running Plexorcist for the first time please read the [Documentation](#documentation).
 
-
 ## Documentation
 
 [Documentation](https://github.com/stefanbc/Plexorcist/wiki)
-
 
 ## Contributing
 
@@ -66,11 +62,9 @@ See [CONTRIBUTING.md](https://github.com/stefanbc/Plexorcist/blob/main/CONTRIBUT
 
 Please adhere to this project's [code of conduct](https://github.com/stefanbc/Plexorcist/blob/main/CODE_OF_CONDUCT.md).
 
-
 ## Authors
 
 - [@stefanbc](https://www.github.com/stefanbc)
-
 
 ## License
 
--- plexorcist.py
+++ plexorcist.py
@@ -1,67 +1,78 @@
 #!/usr/bin/env python
+"""Main Plexorcist execution file!"""
 
 import os
 import sys
-import requests
 import json
-import xmltodict
 import urllib.parse
 from datetime import datetime
+import requests
+import xmltodict
 
 # Check if cast is triggered
-cast = "--cast" in sys.argv
+CAST = "--cast" in sys.argv
 
 # Read the JSON config file
-with open('plexorcist.config.json', 'r') as config_file:
+with open("plexorcist.config.json", "r", encoding="utf8") as config_file:
     # Load the JSON data into a Python dictionary
     config = json.load(config_file)
 
 # Set the script properties
 PLEX_HOSTNAME = f"http://{config['PLEX_HOSTNAME']}:{config['PLEX_PORT']}"
-PLEX_TOKEN = config['PLEX_TOKEN']
-PLEX_LIBRARY = config['PLEX_LIBRARY']
-IFTTT_WEBHOOK = config['IFTTT_WEBHOOK']
-WHITELIST = config['WHITELIST']
-I18N = config['I18N']
+PLEX_TOKEN = config["PLEX_TOKEN"]
+PLEX_LIBRARY = config["PLEX_LIBRARY"]
+IFTTT_WEBHOOK = config["IFTTT_WEBHOOK"]
+WHITELIST = config["WHITELIST"]
+I18N = config["I18N"]
 
 # Set the log file name
-LOG_FILE = 'plexorcist.log'
+LOG_FILE = "plexorcist.log"
 
 # Create file if it's missing
 if not os.path.isfile(LOG_FILE):
     # Create an empty file
-    open(LOG_FILE, 'w').close()
+    with open(LOG_FILE, "w", encoding="utf8") as log_file:
+        log_file.close()
 
 # Size check
-LOG_FILE_MAX_SIZE = 2000000 # 2 MB
+LOG_FILE_MAX_SIZE = 2000000  # 2 MB
 LOG_FILE_CURRENT_SIZE = os.path.getsize(LOG_FILE)
 
 # Check if the log file is larger than LOG_FILE_MAX_SIZE and empty it
 if LOG_FILE_CURRENT_SIZE > LOG_FILE_MAX_SIZE:
-    with open(LOG_FILE, "w") as log_file:
+    with open(LOG_FILE, "w", encoding="utf8") as log_file:
         log_file.truncate(0)
 
 # Set the current timestamp
 TIMESTAMP = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
-# Main plexorcise method
-def plexorcise():
-    # Fetch the Plex data
-    response = make_request(url=f"{PLEX_HOSTNAME}/library/sections/{PLEX_LIBRARY}/allLeaves", headers={"X-Plex-Token": PLEX_TOKEN})
 
-    if response != None:
+def plexorcise():
+    """Main plexorcise method"""
+
+    # Fetch the Plex data
+    response = make_request(
+        url=f"{PLEX_HOSTNAME}/library/sections/{PLEX_LIBRARY}/allLeaves",
+        headers={"X-Plex-Token": PLEX_TOKEN},
+    )
+
+    if response is not None:
         data = xmltodict.parse(response)
-        videos = data['MediaContainer']['Video']
-        media_type = data['MediaContainer']['@viewGroup']
+        videos = data["MediaContainer"]["Video"]
+        media_type = data["MediaContainer"]["@viewGroup"]
 
         if videos and len(videos) > 0:
             watched_videos = []
 
             # Filter watched videos
             if isinstance(videos, list):
-                watched_videos = [video for video in videos if video.get('@viewCount') and int(video['@viewCount']) >= 1]
+                watched_videos = [
+                    video
+                    for video in videos
+                    if video.get("@viewCount") and int(video["@viewCount"]) >= 1
+                ]
             else:
-                if videos.get('@viewCount') and int(videos['@viewCount']) >= 1:
+                if videos.get("@viewCount") and int(videos["@viewCount"]) >= 1:
                     watched_videos.append(videos)
 
             # Delete watched videos and send notification
@@ -71,70 +82,96 @@ def plexorcise():
 
                 # Delete watched videos if not included in WHITELIST
                 for video in watched_videos:
-                    series = video['@grandparentTitle'] if video.get('@grandparentTitle') else ""
-                    title = f"{series} - {video['@title']}" if media_type == "show" else video['@title']
-                    size_in_bytes = int(video['Media']['Part']['@size'])
+                    series = (
+                        video["@grandparentTitle"]
+                        if video.get("@grandparentTitle")
+                        else ""
+                    )
+                    title = (
+                        f"{series} - {video['@title']}"
+                        if media_type == "show"
+                        else video["@title"]
+                    )
+                    size_in_bytes = int(video["Media"]["Part"]["@size"])
                     size_in_mb = size_in_bytes / (1024 * 1024)
 
                     if series not in WHITELIST or title not in WHITELIST:
-                        url = PLEX_HOSTNAME + video['@key']
+                        url = PLEX_HOSTNAME + video["@key"]
 
                         watched_titles.append(title)
                         space_reclaimed_in_mb += round(size_in_mb, 2)
-                        make_request(url=url, headers={"X-Plex-Token": PLEX_TOKEN}, request_type="delete")
+                        make_request(
+                            url=url,
+                            headers={"X-Plex-Token": PLEX_TOKEN},
+                            request_type="delete",
+                        )
                     else:
-                        with open(LOG_FILE, 'a') as log_file:
-                            log_file.write(f"{I18N['WHITELISTED'].format(TIMESTAMP, title)}\n")
+                        with open(LOG_FILE, "a", encoding="utf8") as log_file:
+                            log_file.write(
+                                f"{I18N['WHITELISTED'].format(TIMESTAMP, title)}\n"
+                            )
 
                 space_reclaimed_in_gb = round(space_reclaimed_in_mb / 1024, 2)
 
                 # Write to log file
-                with open(LOG_FILE, 'a') as log_file:
-                    log_file.write(f"{I18N['WATCHED_VIDES_REMOVED'].format(TIMESTAMP, len(watched_videos), space_reclaimed_in_gb)}\n")
-                    log_file.write('\n'.join(watched_titles))
-                    log_file.write('\n')
+                with open(LOG_FILE, "a", encoding="utf8") as log_file:
+                    log_file.write(
+                        f"{I18N['WATCHED_VIDES_REMOVED'].format(TIMESTAMP, len(watched_videos), space_reclaimed_in_gb)}\n"
+                    )
+                    log_file.write("\n".join(watched_titles))
+                    log_file.write("\n")
 
                 # Send notification if IFTTT URL is set
                 webhook_url = urllib.parse.urlparse(IFTTT_WEBHOOK)
-                if "maker.ifttt.com" in IFTTT_WEBHOOK and webhook_url.scheme and webhook_url.netloc:
+                if (
+                    "maker.ifttt.com" in IFTTT_WEBHOOK
+                    and webhook_url.scheme
+                    and webhook_url.netloc
+                ):
                     notification = {
-                        'value1': f"{I18N['WATCHED_VIDES_REMOVED'].format(TIMESTAMP, len(watched_videos), space_reclaimed_in_gb)}\n" + '\n'.join(watched_titles)
+                        "value1": f"{I18N['WATCHED_VIDES_REMOVED'].format(TIMESTAMP, len(watched_videos), space_reclaimed_in_gb)}\n"
+                        + "\n".join(watched_titles)
                     }
-                    make_request(url=IFTTT_WEBHOOK, json=notification, request_type="post")
+                    make_request(
+                        url=IFTTT_WEBHOOK, json=notification, request_type="post"
+                    )
 
-                    with open(LOG_FILE, 'a') as log_file:
+                    with open(LOG_FILE, "a", encoding="utf8") as log_file:
                         log_file.write(f"{I18N['NOTIFICATION'].format(TIMESTAMP)}\n")
                 else:
-                    with open(LOG_FILE, 'a') as log_file:
+                    with open(LOG_FILE, "a", encoding="utf8") as log_file:
                         log_file.write(f"{I18N['IFTTT_ERROR'].format(TIMESTAMP)}\n")
             else:
-                with open(LOG_FILE, 'a') as log_file:
+                with open(LOG_FILE, "a", encoding="utf8") as log_file:
                     log_file.write(f"{I18N['NO_VIDEOS'].format(TIMESTAMP)}\n")
 
             # Open the log file in read mode
-            if cast:
-                with open(LOG_FILE, 'r') as log_file:
+            if CAST:
+                with open(LOG_FILE, "r", encoding="utf8") as log_file:
                     # Read the contents of the log file
                     contents = log_file.read()
                     # Print the contents to the console
                     print(contents)
 
+
 def make_request(**kwargs):
-    url = kwargs.get('url')
-    headers = kwargs.get('headers')
-    json = kwargs.get('json')
-    request_type = kwargs.get('request_type', 'get')
+    """Make a request method"""
+
+    request_url = kwargs.get("url")
+    request_headers = kwargs.get("headers")
+    request_json = kwargs.get("json")
+    request_type = kwargs.get("request_type", "get")
 
     exception = ""
     response = None
 
     try:
-        if request_type == 'delete':
-            response = requests.delete(url, headers=headers)
-        elif request_type == 'post':
-            response = requests.post(url, json=json)
+        if request_type == "delete":
+            response = requests.delete(request_url, headers=request_headers, timeout=10)
+        elif request_type == "post":
+            response = requests.post(request_url, json=request_json, timeout=10)
         else:
-            response = requests.get(url, headers=headers)
+            response = requests.get(request_url, headers=request_headers, timeout=10)
         response.raise_for_status()  # Raise an exception for non-2xx responses
         return response.content
     except requests.exceptions.HTTPError as err:
@@ -147,13 +184,14 @@ def make_request(**kwargs):
         exception = f"{I18N['RequestException'].format(err)}"
 
     if exception != "":
-        if cast:
+        if CAST:
             print(exception)
         else:
-            with open(LOG_FILE, 'a') as log_file:
+            with open(LOG_FILE, "a") as log_file:
                 log_file.write(f"{TIMESTAMP} - {exception}\n")
 
         return None
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     plexorcise()
