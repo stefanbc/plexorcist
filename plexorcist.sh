a81b65695a09ca2453a0bbda1bc8a3d0790dad97
Stefan Cosma <stefcosma@gmail.com>
2023-05-08 17:00:34 +0300

    Visual updates for the installer

--- plexorcist.sh
+++ plexorcist.sh
@@ -7,35 +7,35 @@ VERSION=$(curl -s https://api.github.com/repos/stefanbc/plexorcist/releases/late
 if [[ $1 == "--check" ]]; then
   INSTALLED_VERSION=$(grep -oE "[0-9]+\.[0-9]+\.[0-9]+" plexorcist/plexorcist.py)
   if [[ $INSTALLED_VERSION == $VERSION ]]; then
-    echo "${GREEN}Step 1: Plexorcist is up to date.${RESET}"
+    echo "${GREEN}Step 1:${RESET} Plexorcist is up to date!"
   else
-    echo "${GREEN}Step 1: A new version of Plexorcist is available. Current version: $INSTALLED_VERSION. Latest version: $VERSION.${RESET}"
+    echo "${GREEN}Step 1:${RESET} A new version of Plexorcist is available! Current version: ${GREEN}$INSTALLED_VERSION${RESET}. Latest version: ${GREEN}$VERSION${RESET}."
   fi
   exit 0
 fi
 
-echo "${GREEN}Step 1: Downloading Plexorcist v${VERSION}...${RESET}"
+echo "${GREEN}Step 1:${RESET} Downloading Plexorcist ${GREEN}v${VERSION}${RESET} ..."
 wget -q https://github.com/stefanbc/plexorcist/archive/refs/tags/v${VERSION}.zip -O "plexorcist-${VERSION}.zip"
 
-echo "${GREEN}Step 2: Unzipping plexorcist-${VERSION}.zip...${RESET}"
+echo "${GREEN}Step 2:${RESET} Unzipping ${GREEN}plexorcist-${VERSION}.zip${RESET} ..."
 unzip -qq ./"plexorcist-${VERSION}.zip"
 
-echo "${GREEN}Step 3: Removing plexorcist-${VERSION}.zip...${RESET}"
+echo "${GREEN}Step 3:${RESET} Removing ${GREEN}plexorcist-${VERSION}.zip${RESET} ..."
 rm ./"plexorcist-${VERSION}.zip"
 
 if [[ $1 == "--update" ]]; then
-  echo "${GREEN}Step 4: Updating plexorcist files...${RESET}"
+  echo "${GREEN}Step 4:${RESET} Updating plexorcist files ..."
   cp -rf ./plexorcist-${VERSION}/* ./plexorcist/
 else
-  echo "${GREEN}Step 4: Renaming plexorcist-${VERSION} to plexorcist...${RESET}"
+  echo "${GREEN}Step 4:${RESET} Renaming ${GREEN}plexorcist-${VERSION}${RESET} to ${GREEN}plexorcist${RESET} ..."
   mv -f ./"plexorcist-${VERSION}" ./plexorcist
 fi
 
-echo "${GREEN}Step 5: Installing dependencies using pip...${RESET}"
+echo "${GREEN}Step 5:${RESET} Installing dependencies using pip ..."
 cd plexorcist
 pip install -qq -r requirements.txt
 
-echo "${GREEN}Step 6: Making plexorcist.py executable...${RESET}"
+echo "${GREEN}Step 6:${RESET} Making plexorcist.py executable ..."
 chmod +x plexorcist.py
 
-echo "${GREEN}Step 7: Installation complete!${RESET}"
+echo "${GREEN}Step 7:${RESET} Installation complete!"
