699ea8dff4bbf7e40ef056c3d8444fb1517312a4
Stefan Cosma <stefcosma@gmail.com>
2023-05-08 16:47:30 +0300

    Fixes the install file

--- plexorcist.sh
+++ plexorcist.sh
@@ -1,34 +1,41 @@
 #!/bin/bash
 
-echo -e "\e[32mStep 1:\e[0m Fetching the latest release information from GitHub..."
-version=$(curl -s https://api.github.com/repos/stefanbc/plexorcist/releases/latest | grep -oP '"tag_name": "\K(.*)(?=")')
-echo "Latest version: $version"
+GREEN=$(tput setaf 2)
+RESET=$(tput sgr0)
+VERSION=$(curl -s https://api.github.com/repos/stefanbc/plexorcist/releases/latest | grep -o '"tag_name": "[^"]*"' | grep -oE "[0-9]+\.[0-9]+\.[0-9]+")
 
-echo -e "\e[32mStep 2:\e[0m Downloading Plexorcist v${version}..."
-wget -q https://github.com/stefanbc/plexorcist/archive/refs/tags/v${version}.zip -O "plexorcist-${version}.zip"
-
-echo -e "\e[32mStep 3:\e[0m Unzipping plexorcist-${version}.zip..."
-unzip -q ./"plexorcist-${version}.zip"
-
-echo -e "\e[32mStep 4:\e[0m Removing plexorcist-${version}.zip..."
-rm -f ./"plexorcist-${version}.zip"
-
-update=$1
-
-if [ "$update" = "update" ]; then
-    echo -e "\e[32mStep 5:\e[0m Updating plexorcist..."
-    cp -rf "./plexorcist-${version}/*" ./plexorcist/
-else
-    echo -e "\e[32mStep 5:\e[0m Renaming plexorcist-${version} to plexorcist..."
-    mv -f ./"plexorcist-${version}" ./plexorcist
+if [[ $1 == "--check" ]]; then
+  INSTALLED_VERSION=$(grep -oE "[0-9]+\.[0-9]+\.[0-9]+" plexorcist/plexorcist.py)
+  if [[ $INSTALLED_VERSION == $VERSION ]]; then
+    echo "${GREEN}Step 1: Plexorcist is up to date.${RESET}"
+  else
+    echo "${GREEN}Step 1: A new version of Plexorcist is available. Current version: $INSTALLED_VERSION. Latest version: $VERSION.${RESET}"
+  fi
+  exit 0
 fi
 
-echo -e "\e[32mStep 6:\e[0m Installing dependencies using pip..."
-cd ./plexorcist
-pip install -q -r requirements.txt
+echo "${GREEN}Step 1: Downloading Plexorcist v${VERSION}...${RESET}"
+wget -q https://github.com/stefanbc/plexorcist/archive/refs/tags/v${VERSION}.zip -O "plexorcist-${VERSION}.zip"
 
-echo -e "\e[32mStep 7:\e[0m Making plexorcist.py executable..."
-chmod +x ./plexorcist.py
+echo "${GREEN}Step 2: Unzipping plexorcist-${VERSION}.zip...${RESET}"
+unzip -qq ./"plexorcist-${VERSION}.zip"
 
-echo -e "\e[32mStep 8:\e[0m Installation complete!"
+echo "${GREEN}Step 3: Removing plexorcist-${VERSION}.zip...${RESET}"
+rm ./"plexorcist-${VERSION}.zip"
 
+if [[ $1 == "--update" ]]; then
+  echo "${GREEN}Step 4: Updating plexorcist files...${RESET}"
+  cp -rf ./plexorcist-${VERSION}/* ./plexorcist/
+else
+  echo "${GREEN}Step 4: Renaming plexorcist-${VERSION} to plexorcist...${RESET}"
+  mv -f ./"plexorcist-${VERSION}" ./plexorcist
+fi
+
+echo "${GREEN}Step 5: Installing dependencies using pip...${RESET}"
+cd plexorcist
+pip install -qq -r requirements.txt
+
+echo "${GREEN}Step 6: Making plexorcist.py executable...${RESET}"
+chmod +x plexorcist.py
+
+echo "${GREEN}Step 7: Installation complete!${RESET}"
